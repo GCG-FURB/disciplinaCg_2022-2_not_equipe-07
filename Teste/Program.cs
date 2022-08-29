dce30b992b5764d8c5eb53f91ae91da1e7d5c060
Vinicius Pereira Forte <49294032+viniforte@users.noreply.github.com>
2022-08-29 19:35:46 -0300

    new file:   Teste/Program.cs 	new file:   Teste/Teste.csproj

---
+++ Teste/Program.cs
@@ -0,0 +1,6 @@
+﻿using OpenTK;
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+GameWindow window = new GameWindow(600, 600);
+window.Run(1.0/60.0);
